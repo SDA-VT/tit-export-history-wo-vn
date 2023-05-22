f71af1a084ab800bccfba5b4c1501a5e4fde5649
macxko5047 <78531428+macxko5047@users.noreply.github.com>
2023-05-22 16:55:46 +0700

    update Order_qty column 22/05/23

--- pages/component/home.tsx
+++ pages/component/home.tsx
@@ -136,6 +136,11 @@ const columns: GridColDef[] = [
     width: 160,
   },
   {
+    field: "Order_qty",
+    headerName: "Order_qty",
+    width: 160,
+  },
+  {
     field: "OK_qty",
     headerName: "OK_qty",
     width: 160,
@@ -180,18 +185,6 @@ const columns: GridColDef[] = [
     headerName: "LT_number",
     width: 160,
   },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
 ];
 
 export default function Homes() {
